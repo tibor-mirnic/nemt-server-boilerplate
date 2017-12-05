ec29dbbef730f02d4c762b5aab37b192c9373227
Tibor Mirnić <mirnictibor@gmail.com>
2017-12-05 12:03:15 +0100

    readme update

--- server/core/extensions/mongoose.ts
+++ server/core/extensions/mongoose.ts
@@ -1,8 +1,5 @@
 import { Document, DocumentToObjectOptions } from 'mongoose';
 
-// each model has excludeProps? defined as an Array of Strings
-// for additional document processing define processDocument? function in model definition
-
 export interface ITransformOptions<E> extends DocumentToObjectOptions {
   processDocument?: (doc: Document & E, obj: any & E, options: ITransformOptions<E>) => void;
   applyProcessDocumentToAll?: boolean;
