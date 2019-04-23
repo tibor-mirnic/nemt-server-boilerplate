76e891f186e89bc6f01bb2b83d58eaa25f5ea3c2
Mladen Jakovljević <mladen.jakovljevic@ematicsolutions.com>
2019-04-23 22:13:16 +0200

    Added AuthRouter with ability to log in to the app using "/login" route.

---
+++ server/routes/administration/auth.ts
@@ -0,0 +1,48 @@
+import { NextFunction } from 'express';
+
+import { Router } from '../../core/express/router';
+import { Server } from '../../core/server';
+import { IRequest } from '../../core/models/express/request';
+import { IResponse } from '../../core/models/express/response';
+import { TokenRepository } from '../../repositories/token';
+
+export class AuthRouter extends Router {
+
+  tokenRepository: TokenRepository;
+
+  constructor(server: Server) {
+    super(server);
+    this.tokenRepository = new TokenRepository(this.server);
+  }
+
+  initRoutes() {
+    this.router.route('/login')
+      .post(this.server.passport.local, this.authenticate.bind(this));
+  }
+
+  async authenticate(request: IRequest, response: IResponse, next: NextFunction) {
+    try {
+      const user = request.user;
+
+      if (!user) {
+        throw new Error('Not found!');
+      }
+
+      const dbToken = await this.tokenRepository.create(token => {
+        token.user = user._id.toString();
+        token.type = 'admin';
+        token.expireAt = new Date(Date.now() + 5 * 24 * 3600 * 1000);
+      });
+
+      delete user.passwordHash;
+
+      response.data = {
+        'user': user,
+        'accessToken': dbToken.token
+      };
+      next();
+    } catch (error) {
+      next(Router.handleError(error, request, response));
+    }
+  }
+}
