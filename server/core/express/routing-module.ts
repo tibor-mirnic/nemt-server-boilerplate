ea7f22b50b9f3bf3e4af3e5a1f093bf7385b7fda
Mladen Jakovljević <mladen.jakovljevic@ematicsolutions.com>
2019-04-23 22:23:19 +0200

    RoutingModule: moved variables declarations to constructor.

--- server/core/express/routing-module.ts
+++ server/core/express/routing-module.ts
@@ -2,12 +2,8 @@ import { Server } from '../server';
 import { NotImplementedError } from '../error/user-friendly';
 
 export class RoutingModule {
-  server: Server;
-  baseUrl: string;
 
-  constructor(server: Server, baseUrl: string) {
-    this.server = server;
-    this.baseUrl = baseUrl;
+  constructor(public server: Server, public baseUrl: string) {
   }
 
   build() {
